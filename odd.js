619ad985b4de57ff5d49ea083186bf7cf532a185
eli <eliitheboss@gmail.com>
2022-08-26 22:08:51 +0000

    odd number function practice run

---
+++ odd.js
@@ -0,0 +1,11 @@
+function isOdd(num){
+  var checkNum = (num);
+  if (checkNum % 3 === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
